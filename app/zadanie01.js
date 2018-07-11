3dab129c3d565213120c04db5f8d37e22befab8c
Lucyna Mazur <lucyna@studiokreator.com>
2018-07-11 19:23:53 +0200

    done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,15 @@
+const crypto = require('crypto');
+
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
-//Twój kod
+const passwords = ['??TegoHasła', 'CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n', 'Dzisiaj Szyfruje 83']
+
+const algorithms = ['sha256', 'sha512', 'md5', 'rmd160']
+
+for (password of passwords) {
+	for (algorithm of algorithms) {
+		if (crypto.createHmac(algorithm, password).digest('hex') === MY_PWD_HASH) {
+			console.log(algorithm, password)
+		}
+	}
+}
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,15 @@
 //Twój kod
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const file = process.argv[2];
+
+fs.readFile(file, (err, data) => {
+	if (err===null) {
+		const hash = crypto.createHmac('sha256', data).digest('hex');
+		console.log(hash==='4f7ae6569b55cb6275423ca1cdf31475e607da1d5204c110a58fb480c96e6eca');
+	} else {
+		console.log('błąd podczas odczytu pliku', err);
+	}
+});
